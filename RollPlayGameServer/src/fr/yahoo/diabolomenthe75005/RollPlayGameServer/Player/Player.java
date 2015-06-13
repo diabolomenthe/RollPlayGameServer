e81d8fe807921545c90eb58de28c5e68d58045e5
diabolomenthe75005 <diabolomenthe75005@yahoo.fr>
2015-06-13 17:17:09 +0200

    Version 1

---
+++ RollPlayGameServer/src/fr/yahoo/diabolomenthe75005/RollPlayGameServer/Player/Player.java
@@ -0,0 +1,6 @@
+package fr.yahoo.diabolomenthe75005.RollPlayGameServer.Player;
+
+public class Player {
+	public String message = "Bienvenue sur le serveur" ;
+
+}
---
+++ RollPlayGameServer/src/fr/yahoo/diabolomenthe75005/RollPlayGameServer/RollPlayGameServer.java
@@ -0,0 +1,48 @@
+package fr.yahoo.diabolomenthe75005.RollPlayGameServer;
+
+import java.io.IOException;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.kryonet.Server;
+import com.esotericsoftware.minlog.Log;
+
+import fr.yahoo.diabolomenthe75005.RollPlayGameServer.Player.Player;
+
+public class RollPlayGameServer {
+    Server server;
+    int nbreConnexions = 0;
+    static public final int port = 25565; //le port utilisé pour TCP
+    public RollPlayGameServer () throws IOException {
+         server = new Server(); 
+         Kryo kryo = server.getKryo();
+         kryo.register(Player.class);
+         server.addListener(new Listener()
+         {
+              public void connected (Connection connection){
+                   nbreConnexions = nbreConnexions + 1;
+              }
+              public void received (Connection c, Object object){
+                   if (object instanceof Player)
+                     {
+                       /* on prépare le message de retour à tous les joueurs*/
+                       Player annonce = new Player();
+                       annonce.message = "le joueur : vient de se connecter !";
+                       server.sendToAllTCP(annonce);//on envoie l'objet à tous les joueurs
+                       }
+                   if (object instanceof String){
+                	   System.out.println(object);
+                	   server.sendToAllTCP(object);
+                   }
+               }                 
+         });
+   server.bind(port);
+   server.start();
+    }
+
+   public static void main(String[] args) throws IOException {
+       Log.set(Log.LEVEL_DEBUG);
+       new RollPlayGameServer();
+   }
+}
